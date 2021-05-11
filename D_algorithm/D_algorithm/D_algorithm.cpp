a2f0424fedf8751efdc3f29d62eb7de466b7c6f8
FATBALL <hixuweiyu@126.com>
2021-05-11 15:19:59 +0800

    ADD project of C++

---
+++ D_algorithm/D_algorithm/D_algorithm.cpp
@@ -0,0 +1,14 @@
+// D_algorithm.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello Wasen!" << endl;
+	system("pause");
+    //return 0;
+}
+
